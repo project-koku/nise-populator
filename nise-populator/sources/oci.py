d36494d7e2072e16453ea9bd8d2b6d63a173da79
David N <dnakabaa@redhat.com>
2022-09-20 13:31:08 -0400

    fix linting

--- nise-populator/sources/oci.py
+++ nise-populator/sources/oci.py
@@ -2,14 +2,13 @@ import logging
 import os
 
 from nise.report import oci_create_report
-from oci.config import from_file
 from oci.config import validate_config
 from oci.exceptions import ConfigFileNotFound
 from oci.exceptions import InvalidConfig
 from oci.exceptions import InvalidKeyFilePath
-from oci.exceptions import ProfileNotFound
 
 from sources.source import Source
+
 
 LOG = logging.getLogger(__name__)
 
@@ -17,13 +16,11 @@ LOG = logging.getLogger(__name__)
 class OCI(Source):
     """Defining the OCI source class."""
 
-    BUCKET = "os.object.bucket-name"
-    CONFIG_PROFILE = "NISE_POPULATOR"
-
     def __init__(self, **kwargs):
         """Initialize the source with configuration data."""
         self.kwargs = kwargs
         self.static_file = kwargs.get(self.STATIC_FILE)
+        self.bucket = os.environ.get("OCI_BUCKET_NAME")
         super().__init__(**kwargs)
 
     @staticmethod
@@ -34,34 +31,21 @@ class OCI(Source):
     def check_configuration(self):
         """Determine if source is properly configured for access."""
         try:
-            config = from_file(
-                file_location=os.environ["OCI_CONFIG_FILE"],
-                profile_name=self.CONFIG_PROFILE,
-            )
+            config = {
+                "user": os.environ.get("OCI_CLI_USER"),
+                "fingerprint": os.environ.get("OCI_CLI_FINGERPRINT"),
+                "tenancy": os.environ.get("OCI_CLI_TENANCY"),
+                "key_content": os.environ.get("OCI_CREDENTIALS"),
+                "region": os.environ.get("OCI_REGION"),
+            }
             validate_config(config)
-            if not config.get(self.BUCKET):
-                raise InvalidConfig(
-                    {
-                        self.BUCKET: f"key missing in your config profile {self.CONFIG_PROFILE}"  # noqa: E501
-                    }
-                )
             return True
-        except (ProfileNotFound):
-            LOG.error(
-                f"Profile {self.CONFIG_PROFILE} not found in config file {os.environ['OCI_CONFIG_FILE']}"  # noqa: E501
-            )
-            return False
         except (ConfigFileNotFound, InvalidConfig, InvalidKeyFilePath) as err:
+            LOG.error(f"Provide valid configuration varibales: {err}")
+            return False
+        except Exception as err:
             LOG.error(f"Error : {err}")
             return False
-
-    def _get_bucket_name(self):
-        """Return the OCI object storage bucket name"""
-        config = from_file(
-            file_location=os.environ["OCI_CONFIG_FILE"],
-            profile_name=self.CONFIG_PROFILE,
-        )
-        return config.get(self.BUCKET, "")
 
     def setup(self):
         """Perform necessary setup, like cleaning up existing objectstorage."""
@@ -71,7 +55,7 @@ class OCI(Source):
         options = {
             "start_date": self.start_date,
             "end_date": self.end_date,
-            "oci_bucket_name": self._get_bucket_name(),
+            "oci_bucket_name": self.bucket,
         }
         if self.static_file:
             static_file_data = Source.obtain_static_file_data(
